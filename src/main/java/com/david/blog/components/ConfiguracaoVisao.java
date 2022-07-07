37b153b7dc7364c8ad56f11e82b57c97af055eeb
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2022-07-07 09:36:51 -0300

    Update View

--- src/main/java/com/david/blog/components/ConfiguracaoVisao.java
+++ src/main/java/com/david/blog/components/ConfiguracaoVisao.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ConfiguracaoVisao implements WebMvcConfigurer {
 	
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/auth/user/contato").setViewName("/auth/user/contato");
-		registry.addViewController("/auth/user/curriculo").setViewName("/auth/user/curriculo");
-		registry.addViewController("/auth/user/obrigado").setViewName("/auth/user/obrigado");
-		registry.addViewController("/auth/user/sobre").setViewName("/auth/user/sobre");
-		registry.addViewController("/auth/auth-acesso-negado").setViewName("/auth/auth-acesso-negado");
+		registry.addViewController("auth/user/contato").setViewName("auth/user/contato");
+		registry.addViewController("auth/user/curriculo").setViewName("auth/user/curriculo");
+		registry.addViewController("auth/user/obrigado").setViewName("auth/user/obrigado");
+		registry.addViewController("auth/user/sobre").setViewName("auth/user/sobre");
+		registry.addViewController("auth/auth-acesso-negado").setViewName("auth/auth-acesso-negado");
 	}
 }
