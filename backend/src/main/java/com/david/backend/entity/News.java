993a414c50d9649804a2cd47aa5bc4b16caf1f9c
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2023-12-20 13:32:37 -0300

    Create News Entity

---
+++ backend/src/main/java/com/david/backend/entity/News.java
@@ -0,0 +1,23 @@
+package com.david.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class News {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private String link;
+}
