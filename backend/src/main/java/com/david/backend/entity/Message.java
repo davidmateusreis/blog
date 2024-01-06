da4b4a0c46fe80d155c9da9567ed6846473f5d35
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2024-01-06 13:12:52 -0300

    feat: add form validation

--- backend/src/main/java/com/david/backend/entity/Message.java
+++ backend/src/main/java/com/david/backend/entity/Message.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -19,8 +21,12 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long messageId;
+    @NotBlank(message = "This field may not be blank")
     private String messageAuthor;
+    @Email
+    @NotBlank(message = "This field may not be blank")
     private String messageEmail;
+    @NotBlank(message = "This field may not be blank")
     private String messageContent;
     @CreationTimestamp
     private Instant messageCreatedAt;
--- frontend/src/app/components/your-nintendo-news-contact/your-nintendo-news-contact.component.ts
+++ frontend/src/app/components/your-nintendo-news-contact/your-nintendo-news-contact.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
@@ -14,6 +14,7 @@ export class YourNintendoNewsContactComponent implements OnInit {
   private baseUrl = environment.apiUrl;
 
   contactForm!: FormGroup;
+  submitted = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -21,9 +22,9 @@ export class YourNintendoNewsContactComponent implements OnInit {
     private router: Router
   ) {
     this.contactForm = this.formBuilder.group({
-      messageAuthor: [''],
-      messageEmail: [''],
-      messageContent: ['']
+      messageAuthor: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      messageEmail: ['', [Validators.required, Validators.email, Validators.minLength(4), Validators.maxLength(40)]],
+      messageContent: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(2000)]]
     });
   }
 
@@ -32,6 +33,8 @@ export class YourNintendoNewsContactComponent implements OnInit {
 
   onSubmit() {
     if (this.contactForm.valid) {
+
+      this.submitted = true;
 
       const formData = this.contactForm.value;
 
