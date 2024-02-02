e238c634118bf762831177898387b1541dfa4401
David Mateus <23423789+davidmateusreis@users.noreply.github.com>
2024-02-02 08:18:05 -0300

    chore: set cache expiration

---
+++ backend/src/main/java/com/david/backend/configuration/CacheConfig.java
@@ -0,0 +1,27 @@
+package com.david.backend.configuration;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    @SuppressWarnings("null")
+    CaffeineCacheManager caffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(100));
+
+        return cacheManager;
+    }
+}
